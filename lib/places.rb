1693ddc4ce45d702809bff32ab95c812d04a2847
Julie Turner & Andy Uppendahl <student@epicodus.com>
2015-01-13 11:52:14 -0800

    Modify Place class definition and related spec.

--- lib/places.rb
+++ lib/places.rb
@@ -1,5 +1,25 @@
 class Place
 
-  
+  @@all_places = []
+
+  define_method(:initialize) do |locale|
+    @locale = locale
+  end
+
+  define_method(:locale) do
+    @locale
+  end
+
+  define_method(:save) do
+    @@all_places.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@all_places
+  end
+
+  define_singleton_method(:clear) do
+    @@all_places = []
+  end
 
 end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -1,8 +1,38 @@
 require('rspec')
-require('task')
+require('places')
 
 describe(Place) do
 
+  before() do
+    Place.clear()
+  end
 
+  describe("#locale") do
+    it("allows you to give it a locale representing a place visited") do
+      test_place = Place.new("Greece")
+      expect(test_place.locale()).to(eq("Greece"))
+    end
+  end
 
+  describe(".all") do
+    it("is empty at first") do
+      expect(Place.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a place to the array of saved places") do
+      test_place = Place.new("Italy")
+      test_place.save()
+      expect(Place.all()).to(eq([test_place]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved places") do
+      Place.new("Holland").save()
+      Place.clear()
+      expect(Place.all()).to(eq([]))
+    end
+  end
 end
